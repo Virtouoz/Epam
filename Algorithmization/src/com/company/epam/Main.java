422f7d68a674d341d98c3a18da7c0e33a637ae9f
Aliaksandr <klut2018a@gmail.com>
2021-08-02 17:14:33 +0300

    update

--- Algorithmization/src/com/company/epam/Main.java
+++ Algorithmization/src/com/company/epam/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        task12();
+        task17();
 
 
 /*      Scanner scanner = new Scanner(System.in);
