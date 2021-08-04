0ed32d649e6bbbc45a2ebe88f3e4de8db2abdade
Aliaksandr <klut2018a@gmail.com>
2021-08-04 12:44:48 +0300

    create task 2 Замените в строке все вхождения 'word' на 'letter'.

---
+++ StringsAndBasicsOfTextProcessing/src/com/company/epam/Main.java
@@ -0,0 +1,11 @@
+package com.company.epam;
+
+import static com.company.epam.arrayCharacters.ArrayOfCharacters.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        task2();
+    }
+}
---
+++ StringsAndBasicsOfTextProcessing/src/com/company/epam/arrayCharacters/ArrayOfCharacters.java
@@ -0,0 +1,37 @@
+package com.company.epam.arrayCharacters;
+
+import java.util.Scanner;
+
+public class ArrayOfCharacters {
+
+    // Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+    public static void task1() {
+
+    }
+
+    // Замените в строке все вхождения 'word' на 'letter'.
+    public static void task2() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
+        str = str.replace("word", "letter");
+        System.out.println(str);
+    }
+
+    // В строке найти количество цифр.
+    public static void task3() {
+
+    }
+
+    // В строке найти количество чисел.
+    public static void task4() {
+
+    }
+
+    // Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
+    // Крайние пробелы в строке удалить.
+    public static void task5() {
+
+    }
+
+}
